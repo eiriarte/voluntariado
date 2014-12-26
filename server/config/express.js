178b6c621fe2d6869abcb78b0c8c2c3ef8882976
Eduardo Iriarte Gahete <eiriarte@gmail.com>
2014-12-26 14:46:03 +0100

    Medidas de seguridad

--- server/config/express.js
+++ server/config/express.js
@@ -19,6 +19,9 @@ var passport = require('passport');
 var session = require('express-session');
 var mongoStore = require('connect-mongo')(session);
 var mongoose = require('mongoose');
+var csrf = require('csurf');
+var helmet = require('helmet');
+var errors = require('../components/errors');
 
 module.exports = function(app) {
   var env = app.get('env');
@@ -30,7 +33,7 @@ module.exports = function(app) {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(methodOverride());
-  app.use(cookieParser());
+  app.use(cookieParser(config.secrets.session));
   app.use(passport.initialize());
 
   app.enable('trust proxy');
@@ -40,22 +43,69 @@ module.exports = function(app) {
     secret: config.secrets.session,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: ('production' === env) },
-    store: new mongoStore({ mongoose_connection: mongoose.connection })
+    cookie: { httpOnly: true, secure: ('production' === env) },
+    key: 'session',
+    store: new mongoStore({ mongooseConnection: mongoose.connection })
   }));
 
+  // Protección contra ataques XSRF
+  app.use(csrf());
+  app.use(function(req, res, next) {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    next();
+  });
+
+  // Protección contra ataques XSS
+  var csp = {
+    defaultSrc: [ "'none'" ],
+    scriptSrc: [
+      "'self'",
+      "'unsafe-inline'",
+      "'unsafe-eval'",
+      'https://login.persona.org',
+      'https://ajax.googleapis.com'
+    ],
+    fontSrc: [ "'self'" ],
+    styleSrc: [ "'self'", "'unsafe-inline'" ],
+    connectSrc: [ "'self'" ],
+    imgSrc: [ "'self'", 'data:' ]
+  }
+  if ('development' === env) {
+    // Livereload
+    csp.scriptSrc.push('http://localhost:35729');
+    csp.connectSrc.push('ws://localhost:35729');
+  }
+  app.use(helmet.contentSecurityPolicy(csp));
+  app.use(helmet.xssFilter());
+
+  // Protección contra secuestros de click (Clickjacking)
+  app.use(helmet.frameguard('deny'));
+  // Cabecera HTTP Strict-Transport-Security
+  app.use(helmet.hsts( { maxAge: 10886400000, includeSubdomains: true }));
+  // Ocultar información del software del servidor
+  app.use(helmet.hidePoweredBy({ setTo: 'Enanito con manivela' }));
+  // Desactivar caché
+  app.use(helmet.noCache());
+  // Impedir que el navegador trate de "adivinar" el tipo MIME
+  app.use(helmet.noSniff());
+  // Impedir cargar recursos desde Adobe Flash, entre otros
+  app.use(helmet.crossdomain());
+
   if ('production' === env) {
+    // Forzar conexión segura
     app.use(express_enforces_ssl());
+
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public'), { index: 'noindex' }));
     app.set('appPath', config.root + '/public');
-    app.use(morgan('dev'));
+    app.use(morgan('combined'));
     app.use(errors[500]);
   }
 
   if ('development' === env || 'test' === env) {
     // Simular latencia de BD de 2000ms en desarrollo
     app.use('/api', function(req, res, next) { setTimeout(next, 2000); });
+
     app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp'), { index: 'noindex' }));
     app.use(express.static(path.join(config.root, 'client'), { index: 'noindex' }));
